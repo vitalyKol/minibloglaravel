9cbc33ee8d93a55f7e7325e09061a2529f2ee653
vitalyKol <vental-kvo@yandex.ru>
2022-08-25 10:03:54 +0300

    add about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,21 @@
-<?php
+@extends('layouts.app')
+
+@section('title', 'About me')
+
+@section('content')
+    <h2>About author</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Aspernatur aut consectetur deleniti ducimus ea eum facere
+    fugiat ipsam iusto molestias nesciunt odit repellat, sunt
+        unde voluptate! Illo ipsum repudiandae sed.</p>
+    <p>fugiat ipsam iusto molestias nesciunt odit repellat, sunt
+        unde voluptate! Illo ipsum repudiandae sed.Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Aspernatur aut consectetur deleniti ducimus ea eum facere
+        fugiat ipsam iusto molestias nesciunt odit repellat, sunt
+        unde voluptate! Illo ipsum repudiandae sed.</p>
+    <p>Forem ipsum dolor sit amet, consectetur adipisicing elit.
+        Aspernatur aut consectetur deleniti ducimus ea eum facere
+        fugiat ipsam iusto molestias nesciunt odit repellat, sunt
+        unde voluptate! Illo ipsum repudiandae sed.</p>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('main');
+Route::get('/about', function() {
+    return view('about');
+})->name('about');
